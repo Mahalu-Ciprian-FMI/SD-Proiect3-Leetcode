be9fc73d145b8cd956ee00614995b1dab9a5d153
Mahalu-Ciprian-FMI <128313829+Mahalu-Ciprian-FMI@users.noreply.github.com>
2023-05-09 16:04:47 +0300

    Update deque.cpp

--- deque.cpp
+++ deque.cpp
@@ -1,9 +1,8 @@
-//Problema 7. Deque
-
 #include <iostream>
 #include <cstring>
 #include <queue>
 #include <deque>
+#include <vector>
 #include <fstream>
 using namespace std;
 ifstream f("deque.in");
@@ -11,40 +10,25 @@ ofstream g("deque.out");
 int main()
 {
 	deque<int> dq;
-	vector<int> a;
 	int n, k;
 	f >> n >> k;
+	vector<int> a(n);
 	for (int i = 0;i < n;i++)
 		f >> a[i];
-	int mn = 100000001, sum = 0, ok = 1;
-	for (int i = 0;i < k;i++)
+	long sum = 0;
+	for (int i = 0;i < n;i++)
 	{
-		while (ok)
-		{
-			if (a[i] < a[dq.back()] && !(dq.empty())) //eliminam toate din coada mai mari ca element curent
-				dq.pop_back();
-			else
-				ok = 0;;
-		}
+		while(!(dq.empty()) &&(a[i] <= a[dq.back()]))
+			dq.pop_back();
 		dq.push_back(i);
-	}
-	ok = 1;
-	sum += a[dq.front()];
-	for (int i = k + 1;i < n;i++) // acum acelasi lucru de la pozitile k+1 in sus;
-	{
-		if (i - dq.front() == k)
+		//cout << dq.back() << "back";
+		//cout << dq.front() << "front";
+		if (k == i - dq.front())
 			dq.pop_front();
-		while (ok)
-			{
-			if ((!dq.empty()) && a[dq.back() > a[i]])
-				dq.pop_back();
-			else
-				ok = 0;
-		}
-		dq.push_back(i);
-		sum += a[dq.front()];
+		if (i>=k-1)
+			sum += a[dq.front()];
 	}
-	//cout<<sum;
+	//cout << sum;
 	g << sum;
 	return 0;
 }
