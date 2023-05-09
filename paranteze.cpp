b94ad45e18211b4f80f90b1b7a95082206e96034
Mahalu-Ciprian-FMI <128313829+Mahalu-Ciprian-FMI@users.noreply.github.com>
2023-05-09 12:50:25 +0300

    Update paranteze.cpp

--- paranteze.cpp
+++ paranteze.cpp
@@ -1,32 +1,33 @@
-//Problema 1.Paranteze
 #include <iostream>
 #include <cstring>
 #include <stack>
 #include <fstream>
+#include <vector>
 using namespace std;
 ifstream f("paranteze.in");
 ofstream g("paranteze.out");
-  
+
 int main()
 {
 	int n, mx = 0;
 	string s;
 	stack<int> stiva;
 	f >> n;
-	for (int i = 0;i <n;i++) // i va retine pozitia
+	f >> s;
+	for (int i = 0;i <= n;i++) // i va retine pozitia
 	{
 		if (s[i] == '(' || s[i] == '[' || s[i] == '{')
 			stiva.push(i);
 		else {
-			if ((s[stiva.top()] == '(' && s[i] == ')') || (s[stiva.top()] = '[' && s[i] == ']') || (s[stiva.top()] == '{' && s[i] == '}'))
-			{
-				stiva.pop();
-				if ((i - stiva.top()) > mx) // distanta de pe stiva intre varful stivei si pozitia curenta
-					mx = i - stiva.top();
+				if (((s[stiva.top()] == '(' && s[i] == ')') || (s[stiva.top()] == '[' && s[i] == ']') || (s[stiva.top()] == '{' && s[i] == '}')) && !stiva.empty())
+				{
+					stiva.pop();
+					if ((!stiva.empty() && ((i - stiva.top())) > mx)) // distanta de pe stiva intre varful stivei si pozitia curenta
+						mx = i - stiva.top();
+				}
+				else
+					stiva.push(i);
 			}
-			else
-				stiva.push(i);
-		}
 	}
 	//cout<<mx;
 	g << mx;
